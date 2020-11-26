d945bf55333a719cc0fd92220dcb4749452b80a9
Xeenenta <xeenenta@gmail.com>
2020-11-26 20:34:36 +0000

    Added Sandbag objects

--- reaperCrew_SimpleObjects/config.cpp
+++ reaperCrew_SimpleObjects/config.cpp
@@ -19,13 +19,6 @@ class CfgAddons
 		};
 	};
 };
-class CfgEditorCategories
-{
-	class reaperCrewObjects // Category class, you point to it in editorCategory property
-	{
-		displayName = "Reaper Crew Objects"; // Name visible in the list
-	};
-};
 class CfgEditorSubcategories
 {
 	class reaperCrewFortificationsWoodland // Category class, you point to it in editorSubcategory property
@@ -71,8 +64,20 @@ class CfgVehicles
     class Land_CncWall4_F;
     class Land_Razorwire_F;
 
+	class Land_BagFence_Corner_F;
+	class Land_BagFence_End_F;
+	class Land_BagFence_Long_F;
+	class Land_BagFence_Round_F;
+	class Land_BagFence_Short_F;
+
+	class Land_BagFence_01_corner_green_F;
+	class Land_BagFence_01_end_green_F;
+	class Land_BagFence_01_long_green_F;
+	class Land_BagFence_01_round_green_F;
+	class Land_BagFence_01_short_green_F;
+
     class RC_Land_HBarrier_01_line_3_green_F: Land_HBarrier_01_line_3_green_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsWoodland";
         class SimpleObject
 		{
@@ -85,7 +90,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_HBarrier_01_line_5_green_F: Land_HBarrier_01_line_5_green_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsWoodland";
         class SimpleObject
 		{
@@ -98,7 +103,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_HBarrier_01_big_4_green_F: Land_HBarrier_01_big_4_green_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsWoodland";
         class SimpleObject
 		{
@@ -111,7 +116,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_HBarrier_01_wall_6_green_F: Land_HBarrier_01_wall_6_green_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsWoodland";
         class SimpleObject
 		{
@@ -124,7 +129,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_HBarrier_01_big_tower_green_F: Land_HBarrier_01_big_tower_green_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsWoodland";
         class SimpleObject
 		{
@@ -137,7 +142,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_BagBunker_01_large_green_F: Land_BagBunker_01_large_green_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsWoodland";
         class SimpleObject
 		{
@@ -150,7 +155,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_BagBunker_01_small_green_F: Land_BagBunker_01_small_green_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsWoodland";
         class SimpleObject
 		{
@@ -163,7 +168,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_HBarrier_01_tower_green_F: Land_HBarrier_01_tower_green_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsWoodland";
         class SimpleObject
 		{
@@ -175,9 +180,74 @@ class CfgVehicles
 			init="''";
 		};
     };
-   
+    class RC_Land_BagFence_01_corner_green_F: Land_BagFence_01_corner_green_F {
+        editorCategory = "reaperCrew";
+        editorSubcategory = "reaperCrewFortificationsWoodland";
+        class SimpleObject
+		{
+			eden=1;
+			animate[]={};
+			hide[]={};
+			verticalOffset=0.382000;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+    };
+    class RC_Land_BagFence_01_end_green_F: Land_BagFence_01_end_green_F {
+        editorCategory = "reaperCrew";
+        editorSubcategory = "reaperCrewFortificationsWoodland";
+        class SimpleObject
+		{
+			eden=1;
+			animate[]={};
+			hide[]={};
+			verticalOffset=0.382000;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+    };
+    class RC_Land_BagFence_01_long_green_F: Land_BagFence_01_long_green_F {
+        editorCategory = "reaperCrew";
+        editorSubcategory = "reaperCrewFortificationsWoodland";
+        class SimpleObject
+		{
+			eden=1;
+			animate[]={};
+			hide[]={};
+			verticalOffset=0.382000;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+    };
+    class RC_Land_BagFence_01_round_green_F: Land_BagFence_01_round_green_F {
+        editorCategory = "reaperCrew";
+        editorSubcategory = "reaperCrewFortificationsWoodland";
+        class SimpleObject
+		{
+			eden=1;
+			animate[]={};
+			hide[]={};
+			verticalOffset=0.382000;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+    };
+    class RC_Land_BagFence_01_short_green_F: Land_BagFence_01_short_green_F {
+        editorCategory = "reaperCrew";
+        editorSubcategory = "reaperCrewFortificationsWoodland";
+        class SimpleObject
+		{
+			eden=1;
+			animate[]={};
+			hide[]={};
+			verticalOffset=0.382000;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+    };
+
     class RC_Land_HBarrier_3_F: Land_HBarrier_3_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsDesert";
         class SimpleObject
 		{
@@ -190,7 +260,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_HBarrier_5_F: Land_HBarrier_5_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsDesert";
         class SimpleObject
 		{
@@ -203,7 +273,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_HBarrier_Big_F: Land_HBarrier_Big_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsWoodland";
         class SimpleObject
 		{
@@ -216,7 +286,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_HBarrierWall6_F: Land_HBarrierWall6_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsDesert";
         class SimpleObject
 		{
@@ -229,7 +299,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_HBarrierTower_F: Land_HBarrierTower_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsDesert";
         class SimpleObject
 		{
@@ -242,7 +312,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_BagBunker_Large_F: Land_BagBunker_Large_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsDesert";
         class SimpleObject
 		{
@@ -255,7 +325,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_BagBunker_Small_F: Land_BagBunker_Small_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsDesert";
         class SimpleObject
 		{
@@ -268,7 +338,72 @@ class CfgVehicles
 		};
     };
     class RC_Land_BagBunker_Tower_F: Land_BagBunker_Tower_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
+        editorSubcategory = "reaperCrewFortificationsDesert";
+        class SimpleObject
+		{
+			eden=1;
+			animate[]={};
+			hide[]={};
+			verticalOffset=0.382000;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+    };
+    class RC_Land_BagFence_Corner_F: Land_BagFence_Corner_F {
+        editorCategory = "reaperCrew";
+        editorSubcategory = "reaperCrewFortificationsDesert";
+        class SimpleObject
+		{
+			eden=1;
+			animate[]={};
+			hide[]={};
+			verticalOffset=0.382000;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+    };
+    class RC_Land_BagFence_End_F: Land_BagFence_End_F {
+        editorCategory = "reaperCrew";
+        editorSubcategory = "reaperCrewFortificationsDesert";
+        class SimpleObject
+		{
+			eden=1;
+			animate[]={};
+			hide[]={};
+			verticalOffset=0.382000;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+    };
+    class RC_Land_BagFence_Long_F: Land_BagFence_Long_F {
+        editorCategory = "reaperCrew";
+        editorSubcategory = "reaperCrewFortificationsDesert";
+        class SimpleObject
+		{
+			eden=1;
+			animate[]={};
+			hide[]={};
+			verticalOffset=0.382000;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+    };
+    class RC_Land_BagFence_Round_F: Land_BagFence_Round_F {
+        editorCategory = "reaperCrew";
+        editorSubcategory = "reaperCrewFortificationsDesert";
+        class SimpleObject
+		{
+			eden=1;
+			animate[]={};
+			hide[]={};
+			verticalOffset=0.382000;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+    };
+    class RC_Land_BagFence_Short_F: Land_BagFence_Short_F {
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsDesert";
         class SimpleObject
 		{
@@ -282,7 +417,7 @@ class CfgVehicles
     };
 
     class RC_Land_ConcreteHedgehog_01_F: Land_ConcreteHedgehog_01_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsGeneric";
         class SimpleObject
 		{
@@ -295,7 +430,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_Mil_ConcreteWall_F: Land_Mil_ConcreteWall_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsGeneric";
         class SimpleObject
 		{
@@ -308,7 +443,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_CncBarrier_F: Land_CncBarrier_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsGeneric";
         class SimpleObject
 		{
@@ -321,7 +456,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_CncBarrierMedium_F: Land_CncBarrierMedium_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsGeneric";
         class SimpleObject
 		{
@@ -334,7 +469,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_CncBarrierMedium4_F: Land_CncBarrierMedium4_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsGeneric";
         class SimpleObject
 		{
@@ -347,7 +482,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_CncBarrier_stripes_F: Land_CncBarrier_stripes_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsGeneric";
         class SimpleObject
 		{
@@ -360,7 +495,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_CncWall1_F: Land_CncWall1_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsGeneric";
         class SimpleObject
 		{
@@ -373,7 +508,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_CncWall4_F: Land_CncWall4_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsGeneric";
         class SimpleObject
 		{
@@ -386,7 +521,7 @@ class CfgVehicles
 		};
     };
     class RC_Land_Razorwire_F: Land_Razorwire_F {
-        editorCategory = "reaperCrewObjects";
+        editorCategory = "reaperCrew";
         editorSubcategory = "reaperCrewFortificationsGeneric";
         class SimpleObject
 		{
